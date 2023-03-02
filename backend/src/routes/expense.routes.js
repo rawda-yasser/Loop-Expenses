28ca707fbe036e8454d926e41b7c54fa301f314e
Rawda Yasser <rawda.developer@gmail.com>
2023-03-02 09:32:10 +0200

    Feat: add create expense route

--- backend/src/routes/expense.routes.js
+++ backend/src/routes/expense.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { requireLogin, hasAuthorization } from "../controllers/auth.controller";
-import { listByUser } from "../controllers/expense.controller"
+import { create, listByUser } from "../controllers/expense.controller";
 const router = express.Router();
-router.route("/").get(requireLogin, listByUser);
+router.route("/").get(requireLogin, listByUser).post(requireLogin, create);
 export default router;
