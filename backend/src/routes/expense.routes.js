73e6bfa45082a3bb4b38f0c9830f75323f1bb8ee
Rawda Yasser <rawda.developer@gmail.com>
2023-03-02 10:09:40 +0200

    Feat: add remove expense route for the authorized users

--- backend/src/routes/expense.routes.js
+++ backend/src/routes/expense.routes.js
@@ -6,9 +6,10 @@ import {
   isOwner,
   listByUser,
   update,
+  remove
 } from "../controllers/expense.controller";
 const router = express.Router();
 router.route("/").get(requireLogin, listByUser).post(requireLogin, create);
-router.route("/:expenseId").put(requireLogin, isOwner, update);
+router.route("/:expenseId").put(requireLogin, isOwner, update).delete(requireLogin, isOwner, remove);
 router.param("expenseId", expenseByID);
 export default router;
