8c6e4734d9c54eca5b96b7d3234bcb709f61fdc3
Rawda Yasser <rawda.developer@gmail.com>
2023-03-02 08:56:05 +0200

    Feat: add expenses router to the main server

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,12 @@ import "dotenv/config";
 import config from "./config";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
+import expenseRoutes from "./routes/expense.routes";
 const app = express();
 app.use(express.json());
 app.get("/", (req, res) => res.json({ message: "Loop Expenses" }));
 app.use("/api/users", userRoutes);
+app.use("/api/expenses", expenseRoutes);
 app.use("/auth", authRoutes);
 app.listen(config.port, () => console.log("Listening on port 4000"));
 app.use((err, req, res, next) => {
