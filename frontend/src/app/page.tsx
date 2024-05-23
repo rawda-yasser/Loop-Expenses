d9dc837a9dd090cd65694df455873b08506e6f82
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2024-05-23 22:38:24 +0300

    Feat: add mui setup for frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
-    <main>
-      Loop Expenses
+    <main className={styles.main}>
+      <Button variant="contained">Loop Expenses MUI</Button>
     </main>
   );
 }
